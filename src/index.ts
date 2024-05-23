4bd726f29e0b7b818c48c9582e4a8c626109f6bc
Dmarzzucco <ksksks>
2024-05-23 15:55:55 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
-
+// ->
 const App = express();
 const Port = process.env.PORT || 3000;
-
+// ->
 App.use(cors());
 App.use(express.json());
-
+// ->
 let messages: string[] = [];
-
+// ->
 App.get('/message', (_req, res) => {
     res.json(messages)
 });
 
-App.post('/message', async (req, res) => {
+App.post('/message', (req, res) => {
     const message = req.body.message;
     try {
         if (typeof message === 'string') {
             messages.push(message);
-            res.status(201).send('Message Aded')
+            res.status(201).send('Message Added')
         } else {
             throw new Error('Invalid Message Format')
         }
     } catch (error) {
-        res.status(400).send('Invalid Message Format')
+        res.status(400).send(error instanceof Error ? error.message : 'Uknown Error');
     }
 });
-
+// ->
 App.listen(Port, () => {
     console.log(`Server listen at port ${Port}`)
 })
-
