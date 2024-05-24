857b9b69e41df25d04fbbea330bb6057a526cea7
Dmarzzucco <ksksks>
2024-05-24 15:26:14 -0300

    controllers added

---
+++ src/controllers/task.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { IMessage } from "../interfaces/IMessage";
+
+let Messages: IMessage[] = [];
+
+export const getTask = async (_req: Request, res: Response) => {
+    res.json(Messages);
+    console.log("connect")
+}
+export const postTask = async (req: Request, res: Response) => {
+    const message: IMessage = req.body.message;
+    try {
+        if (typeof message === 'string') {
+            Messages.push(message);
+            console.log(message)
+            return res.status(201).send('Message Added');
+        } else {
+            throw new Error('Invalid Format');
+        }
+    } catch (error) {
+        return res.status(401).send(error instanceof Error ? error.message : 'Uknown Error')
+    }
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+    const index: number = parseInt(req.params.index, 10);
+    try {
+        if (isNaN(index) || index < 0 || index >= Messages.length) {
+            return res.status(400).send('Message not was found');
+        }
+        Messages.splice(index, 1);
+        console.log('DELETE');
+        return res.status(200).send('Message was deleted');
+    } catch (error) {
+        return res.status(401).send(error);
+    }
+}
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,40 +1,10 @@
-import { Router, Request, Response } from "express";
-import { IMessage } from "../interfaces/IMessage";
+import { Router } from "express";
+import { deleteTask, getTask, postTask } from "../controllers/task.controller";
 
 const router = Router();
-let Messages: IMessage[] = [];
 
-router.get('/messages', (_req: Request, res: Response) => {
-    res.json(Messages);
-})
-router.post('/message', (req: Request, res: Response) => {
-    const message: IMessage = req.body.message;
-    try {
-        if (typeof message === 'string') {
-            Messages.push(message);
-            res.status(201).send('Message Added');
-            console.log(message);
-        } else {
-            throw new Error('Invalid Format');
-        }
-    } catch (error) {
-        res.status(401).send(error instanceof Error ? error.message : 'Uknown Error')
-        return;
-    }
-})
-router.delete('/message/:index', (req: Request, res: Response) => {
-    const index: number = parseInt(req.params.index, 10);
-    try {
-        if (isNaN(index) || index < 0 || index >= Messages.length) {
-            return res.status(400).send('Message not Found');
-        }
-        Messages.splice(index, 1);
-        console.log('DELETE');
-        return res.status(200).send('Message was deleted');
-    } catch (error) {
-        res.status(401).send(error);
-        return;
-    }
-})
+router.get("/message", getTask);
+router.post("/message", postTask);
+router.delete("/message/:index", deleteTask);
 
 export default router;
