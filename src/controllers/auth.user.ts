3bd9a77e30269d7b8586483ae806fde940360bf7
DMarzzucco <drrkermazyxokv@gmail.com>
2024-06-24 14:16:17 -0300

    fresh

--- src/controllers/auth.user.ts
+++ src/controllers/auth.user.ts
@@ -64,7 +64,7 @@ export const login = async (req: Request, res: Response) => {
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({ error: error })
+        res.status(500).json({ error: [{error }]})
     }
 }
 
